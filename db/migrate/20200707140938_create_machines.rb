ebab369eb43f42037ad942b30662cacf9a67ada7
渡辺有智 <aritomo@aritomo.local>
2020-07-08 00:45:45 +0900

    fix migrate

--- db/migrate/20200707140938_create_machines.rb
+++ db/migrate/20200707140938_create_machines.rb
@@ -2,6 +2,7 @@ class CreateMachines < ActiveRecord::Migration[5.2]
   def change
     create_table :machines do |t|
 
+    	t.integer :construction_site_id, null: false
     	t.string :name, null: false
     	t.boolean :need_repair, default: false, null: false
 
