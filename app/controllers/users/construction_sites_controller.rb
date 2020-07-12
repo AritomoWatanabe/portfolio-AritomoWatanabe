7f4fca9dec489266b82d69db35d2a964d6c8bab3
渡辺有智 <aritomo@aritomo.local>
2020-07-12 13:42:17 +0900

    Add user mypage & user construction_site

--- app/controllers/users/construction_sites_controller.rb
+++ app/controllers/users/construction_sites_controller.rb
@@ -1,2 +1,13 @@
 class Users::ConstructionSitesController < ApplicationController
+
+	def index
+		@construction_sites = ConstructionSite.all.page(params[:page]).per(10)
+	end
+
+	def show
+		@construction_site = ConstructionSite.find(params[:id])
+		@photos = Photo.all
+	end
+
+
 end
--- app/controllers/users/photos_controller.rb
+++ app/controllers/users/photos_controller.rb
@@ -1,2 +1,22 @@
 class Users::PhotosController < ApplicationController
+
+	def new
+		@photo = Photo.new(photo_params)
+	end
+
+	def create
+		@photo = Photo.new(photo_params)
+		@photo.save
+			redirect_to users_construction_site_path(@construction_site.id)
+	end
+
+	private
+  		def photo_params
+    		params.permit(
+    			:construction_site_id,
+    			:image_id,
+    			:explanation)
+  		end
+
+
 end
