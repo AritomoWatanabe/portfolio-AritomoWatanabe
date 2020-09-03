aa1bdc867c2a597c27e6c67866c5498816644842
渡辺有智 <aritomo@aritomo.local>
2020-09-03 15:36:15 +0900

    Finish about & Fix Readme

--- app/controllers/users/homes_controller.rb
+++ app/controllers/users/homes_controller.rb
@@ -4,6 +4,9 @@ class Users::HomesController < ApplicationController
 	def top
 		@attendance_record = AttendanceRecord.new
 		@daily_report = DailyReport.new
+		wd = ["日", "月", "火", "水", "木", "金", "土"]
+		time = Time.new
+		@date = time.strftime("%Y年 %m月 %d日(#{wd[time.wday]})")
 	end
 
 
